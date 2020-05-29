7accb6a299f1cbab82f63538a3ab247c7f04189e
Carlos Mertens <mertenscarlos@yahoo.com>
2020-05-29 15:26:35 +0200

    Create QuiyType component to modulate the cards

--- src/components/QuizBar.js
+++ src/components/QuizBar.js
@@ -1,27 +1,15 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faDumbbell,
-  faFont,
-  faFileAlt,
-  faDice,
-} from "@fortawesome/free-solid-svg-icons";
-import { library } from "@fortawesome/fontawesome-svg-core";
-library.add(faDumbbell);
-library.add(faFont);
-library.add(faFileAlt);
-library.add(faDice);
+import QuizType from "./QuizType";
 
 function QuizBar(props) {
   return (
     <div className="quiz-bar">
       <h1>Choose your study type</h1>
       <ul className="nav nav-pills nav-fill">
-        <li className="col-sm-3 text-center">
-          <div className="nav-card">
-            <FontAwesomeIcon icon="dice" size="4x" />
-          </div>
-        </li>
+        <QuizType icon="dice" quizType="Random" />
+        <QuizType icon="font" quizType="Multi" />
+        <QuizType icon="file-alt" quizType="Regular" />
+        <QuizType icon="dumbbell" quizType="Weighted" />
       </ul>
     </div>
   );
---
+++ src/components/QuizType.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDumbbell,
+  faFont,
+  faFileAlt,
+  faDice,
+} from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+library.add(faDumbbell);
+library.add(faFont);
+library.add(faFileAlt);
+library.add(faDice);
+
+function QuizType(props) {
+  return (
+    <li className="col-sm-3 text-center">
+      <div className="nav-card">
+        <FontAwesomeIcon icon={props.icon} size="4x" />
+        <span>{props.quizType}</span>
+      </div>
+    </li>
+  );
+}
+
+export default QuizType;
