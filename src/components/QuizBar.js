c806d1c27b1aadb005e9ab84ec39aa14c425a2f4
Carlos Mertens <mertenscarlos@yahoo.com>
2020-05-29 16:39:42 +0200

    Refactor QuizBar component to concise code

--- src/components/QuizBar.js
+++ src/components/QuizBar.js
@@ -2,23 +2,30 @@ import React from "react";
 import QuizType from "./QuizType";
 
 function QuizBar(props) {
+  const quizArray = [
+    { icon: "dice", type: "Random" },
+    { icon: "file-alt", type: "Regular" },
+    { icon: "dumbbell", type: "Weighted" },
+    { icon: "font", type: "Multi" },
+  ];
+
+  // Map through the array and create a quiz type crad with QuizType component
+  // Pass through the props
+  const quizTypes = quizArray.map((typeItem, index) => {
+    return (
+      <QuizType
+        key={index}
+        icon={typeItem.icon}
+        quizType={typeItem.type}
+        userChoice={props.userChoice}
+      />
+    );
+  });
+
   return (
     <div className="quiz-bar">
       <h1>Choose your study type</h1>
-      <ul className="nav nav-pills nav-fill">
-        <QuizType icon="dice" quizType="Random" userChoice={props.userChoice} />
-        <QuizType icon="font" quizType="Multi" userChoice={props.userChoice} />
-        <QuizType
-          icon="file-alt"
-          quizType="Regular"
-          userChoice={props.userChoice}
-        />
-        <QuizType
-          icon="dumbbell"
-          quizType="Weighted"
-          userChoice={props.userChoice}
-        />
-      </ul>
+      <ul className="nav nav-pills nav-fill">{quizTypes}</ul>
     </div>
   );
 }
