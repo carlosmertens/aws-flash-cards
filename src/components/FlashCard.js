19e48ead6b7db6e1170680fa7d12c0480a308905
Carlos Mertens <mertenscarlos@yahoo.com>
2020-05-29 16:56:16 +0200

    Setup and Sanity Check MultiCard component

--- src/components/FlashCard.js
+++ src/components/FlashCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import RandomWeighted from "./RandomWeighted";
 import RegularCard from "./RegularCard";
+import MultiCard from "./MultiCard";
 
 class FlashCard extends Component {
   constructor(props) {
@@ -13,6 +14,7 @@ class FlashCard extends Component {
         <h1>Flash Card Component</h1>
         <RandomWeighted />
         <RegularCard />
+        <MultiCard />
       </div>
     );
   }
---
+++ src/components/MultiCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MultiCard(props) {
+  return (
+    <div>
+      <h3>MultiCard Component</h3>
+    </div>
+  );
+}
+
+export default MultiCard;
