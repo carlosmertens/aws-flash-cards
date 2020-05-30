a870ee6f4f57ceb9dc99d8b41d2fc2c08913f90a
Carlos Mertens <mertenscarlos@yahoo.com>
2020-05-30 02:15:36 +0200

    Updates

---
+++ precache-manifest.7a935856daeaa2a6ed657ee23d984f9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb57cec25a4ecaa66d971af8efd9ead6",
+    "url": "/aws-flash-cards/index.html"
+  },
+  {
+    "revision": "f0eb3b540d7908994895",
+    "url": "/aws-flash-cards/static/css/main.a268d6a1.chunk.css"
+  },
+  {
+    "revision": "934134227e18290c7325",
+    "url": "/aws-flash-cards/static/js/2.345686e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aws-flash-cards/static/js/2.345686e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0eb3b540d7908994895",
+    "url": "/aws-flash-cards/static/js/main.b54ab97d.chunk.js"
+  },
+  {
+    "revision": "a8dfd3a5298b5dade935",
+    "url": "/aws-flash-cards/static/js/runtime-main.829ba8d5.js"
+  }
+]);
